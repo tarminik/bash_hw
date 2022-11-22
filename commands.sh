ac750bc864fd81e0f28a53946316c51be47fc374
Tarasov Mikhail <tarasov.mn@phystech.edu>
2022-11-22 23:26:30 +0300

    foolproof

--- commands.sh
+++ commands.sh
@@ -7,9 +7,28 @@ cat << EOF
 4. ps -fe
 EOF
 read num
-case $num in
-	1) ls;;
-	2) pwd;;
-	3) ls -l;;
-	4) ps -fe;;
-esac
+while true
+do
+	case $num in
+		1)
+			ls
+			break
+			;;
+		2)
+			pwd
+			break
+			;;
+		3)
+			ls -l
+			break
+			;;
+		4)
+			ps -fe
+			break
+			;;
+		*)
+			echo Введите число от 1 до 4
+			read num
+			;;
+	esac
+done
