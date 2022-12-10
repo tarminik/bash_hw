196c5699baca99c33947e4ada243b23403e02c7a
Mikhail Tarasov <tarasov.mn@phystech.edu>
2022-12-10 17:38:25 +0300

    fixed

--- archive.sh
+++ archive.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
+export ARCHIVE_PATH=/home/tarminik/Desktop
 find ~/Downloads -mtime -1 > files.txt
 echo "$(tail -n +2 files.txt)" > files.txt
 dat=`date +%F`
 tar -cf archive_${dat}.tar.gz -T files.txt
-
+mv archive_${dat}.tar.gz $ARCHIVE_PATH
